99b6c54ac68982f789d10b1fcd0f597cb3d58fe4
Omar Santos <contacto@omarsantos.dev>
2026-03-01 12:45:51 +0100

    refactor(config.service): Add destroy subscriptions

--- src/app/core/services/config.service.ts
+++ src/app/core/services/config.service.ts
@@ -1,14 +1,19 @@
-import {Injectable} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import {Injectable, OnDestroy} from '@angular/core';
+import {BehaviorSubject, Subject, takeUntil} from 'rxjs';
 import {filter} from 'rxjs/operators';
 
 import {DashboardConfig} from '../models/dashboard.models';
 
 @Injectable({ providedIn: 'root' })
-export class ConfigService {
+export class ConfigService implements OnDestroy {
   private configSubject = new BehaviorSubject<DashboardConfig | undefined>(undefined);
 
-  readonly config$ = this.configSubject.asObservable().pipe(filter((config) => !!config));
+  destroy$: Subject<void> = new Subject<void>();
+
+  readonly config$ = this.configSubject.asObservable().pipe(
+    takeUntil(this.destroy$),
+    filter((config) => !!config)
+  );
 
   get subject(): BehaviorSubject<DashboardConfig | undefined> {
     return this.configSubject;
@@ -16,5 +21,9 @@ export class ConfigService {
 
   fireNewSubject(config: DashboardConfig): void {
     this.configSubject.next(config);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
   }
 }
